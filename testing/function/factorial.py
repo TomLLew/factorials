06f76732ab00a73c663821c66a4e86fbd382a81c
TomLLew <thomas.llewelyn@academytrainee.com>
2019-11-26 14:46:17 +0000

    function commit

---
+++ testing/function/factorial.py
@@ -0,0 +1,15 @@
+def factorial(num):
+    n = num
+    factorial = 1
+    while n > 1:
+        n = n / factorial
+        factorial += 1
+    if n == 1:
+        return factorial - 1
+    else:
+        return 'none'
+
+
+
+    
+    
