b0cada1c2d9f09f153c168c0ba8e27736c856e79
TomLLew <thomas.llewelyn@academytrainee.com>
2019-11-26 13:05:36 +0000

    factorial tests

---
+++ testing/tests/factorial_tests.py
@@ -0,0 +1,22 @@
+import pytest
+from function import factorial
+
+def test_factorial01():
+    assert factorial.factorial(6)==3
+
+def test_factorial02():
+    assert factorial.factorial(18)=='none'
+
+def test_factorial03():
+    assert factorial.factorial(120)==5
+
+def test_factorial04():
+    assert factorial.factorial(150)=='none'
+
+def test_factorial05():
+    assert factorial.factorial(3628800)==10
+
+def test_factorial06():
+    assert factorial.factorial(479001600)==12
+
+
